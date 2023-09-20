98b45ff383ec568bf2093ae4ef301aa2b1b88d7e
Alex Wilson <alex.wilson.0425@gmail.com>
2023-09-19 23:57:55 -0500

    Added games file and route

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/aboutme', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'views','aboutme.html'));
 });
 
+app.get('/software', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views','software.html'));
+});
+
+app.get('/games', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views','games.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 });
