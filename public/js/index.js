8c7d0e3da51e1ad57dba82a908fd592bd1e940cb
Alex Wilson <alex.wilson.0425@gmail.com>
2023-10-10 16:43:20 -0500

    Switched to pug, Contact button works

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,19 @@
-console.log("Working console.log in the browser!1!")
+//Identify DOM element
+let composeButton = document.getElementById('contact-me-button');// Function to handle the button click
+
+
+function composeEmail() {
+    // Define the email address, subject, and body
+    let emailAddress = 'alex.wilson.0425@gmail.com';
+    let subject = 'Security Vulnerability/Bug Report!';
+    let body = '';
+
+    // Create a mailto link with the email address, subject, and body
+    let mailtoLink = 'mailto:' + emailAddress + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+
+    // Open the default email client with the mailto link
+    window.location.href = mailtoLink;
+}
+
+//Add event listener to previously selected DOM element 
+composeButton.addEventListener('click', composeEmail);
--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
 
+/*
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 
 app.get('/aboutme', (req, res) => {
@@ -28,6 +30,31 @@ app.get('/projects', (req, res) => {
 
 app.get('/resources', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'views','resources.html'));
+});
+*/
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'views', 'aboutme.pug'))
+});
+
+app.get('/aboutme', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'views', 'aboutme.pug'))
+});
+
+app.get('/software', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'views', 'software.pug'))
+});
+
+app.get('/media', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'views', 'media.pug'))
+});
+
+app.get('/projects', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'views', 'projects.pug'))
+});
+
+app.get('/resources', (req, res) => {
+  res.render(path.join(__dirname, 'public', 'views', 'resources.pug'))
 });
 
 
